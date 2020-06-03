b5518fb561c0f0bca8581ff21c7d7ae96dcc6a97
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-03 18:40:41 +0000

    Automatically backed up by Learn

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO cats (name, age, breed) VALUES ('Lil" Bub', 5, 'American Shorthair');
+INSERT INTO cats (name, age, breed) VALUES ('Hannah', 1, 'Tabby');
+sqlite> INSERT INTO cats (name, age, breed, owner_id) VALUES ("Maru", 3 , "Scottish Fold", 1);
+sqlite> INSERT INTO cats (name, age, breed, owner_id) VALUES ("Hana", 1 , "Tabby", 1);
+sqlite> INSERT INTO cats (name, age, breed) VALUES ("Lil\' Bub", 5, "American Shorthair");
+sqlite> INSERT INTO cats (name, age, breed) VALUES ("Moe", 10, "Tabby");
+sqlite> INSERT INTO cats (name, age, breed) VALUES ("Patches", 2, "Calico");
